f35cabfa22f6639ef0a391bb099dbece8e6fbca0
majednitol <majadrahman7@gmail.com>
2025-11-06 22:41:48 +0600

    done

--- src/Video.js
+++ src/Video.js
@@ -20,7 +20,7 @@ import Modal from 'react-bootstrap/Modal'
 import 'bootstrap/dist/css/bootstrap.css'
 import "./Video.css"
 
-const server_url = process.env.NODE_ENV === 'production' ? 'https://backend-video-meeting-l3tv.onrender.com' : "http://localhost:4001"
+const server_url = process.env.NODE_ENV === 'production' ? 'https://backend-video-meeting-1-kva9.onrender.com' : "http://localhost:4001"
 
 var connections = {}
 const peerConnectionConfig = {
